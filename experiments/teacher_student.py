118e58ed201465ab3467aeaa01255c60ec7982fa
Rong Xiao <REDMOND.rxiao@VIGDGX01.redmond.corp.microsoft.com>
2018-04-20 15:07:44 -0700

    remove .pyc

--- experiments/teacher_student.py
+++ experiments/teacher_student.py
@@ -9,37 +9,39 @@ class DarkTeachSwiftNet(NeuralNetwork):
         datalayer = self.bottom;
         teacher = DarkNet('teacher', basemodel = self)
         teacher.backbone()
-        yolo_addextra(teacher, nclass)
+        yolo_addextra(teacher, nclass,yolostyle=True)
         teacher.set_conv_params()        
         blacklist = teacher.fix_params();
         self.set_bottom(datalayer)
         student = SwiftNet('student',basemodel = self, namespace='s')
         student.backbone()
-        yolo_addhead(student,nclass, deploy=False)
+        #yolo_addhead(student,nclass, deploy=False,head_cfg = [(512,1,1), (1024,3,8)]*2)
+        yolo_addextra(student,nclass, head_cfg = [(512,1,1), (1024,3,8)]*2)
         return blacklist
 
-def yolo_ts(nclass):
+def yolo_ts0(nclass):
+    name = 'dark-swiftnet'
+    trainnet = DarkTeachSwiftNet(name)
+    datafolder = 'data'
+    imgsize = 416
+    trainnet.tsv_inception_layer("tsv480/train.resize480.shuffled.tsv",224,batchsize=(64,50),new_image_size =(256,256),phases=[caffe.TRAIN,caffe.TEST])
+    blacklist=trainnet.backbone(nclass, deploy=False)
+    trainnet.euclideanloss('s_yolo_4/conv','extra_conv21/conv')
+    trainnet.set_conv_params(blacklist=blacklist)
+    trainnet.silence('label','s_last_conv','last_conv')
+    with open('train.prototxt','w') as fout:
+        fout.write(trainnet.toproto());
+def yolo_ts1(nclass):
     name = 'dark-swiftnet'
     trainnet = DarkTeachSwiftNet(name)
     datafolder = 'data'
     imgsize = 416
     yolo_addinput(trainnet,datafolder,nclass,imgsize,deploy=False)
     blacklist=trainnet.backbone(nclass, deploy=False)
-    trainnet.euclideanloss('s_yolo_3/conv','yolo_3/conv')
+    trainnet.euclideanloss('s_yolo_4/conv','extra_conv21/conv'，loss_weight=0.1)
     trainnet.set_conv_params(blacklist=blacklist)
+    trainnet.silence('label','s_last_conv','last_conv')
     with open('train.prototxt','w') as fout:
         fout.write(trainnet.toproto());
-'''
-def classification_ts(nclass,imgsize,batchsize,**kwargs):
-    name = 'tinydarknet_ts'
-    trainnet = TinyDarkNetTS(name)
-    trainnet.tsv_inception_layer("tsv480/train.resize480.shuffled.tsv",imgsize[0],batchsize=batchsize,new_image_size =(imgsize[1],imgsize[1]),phases=[caffe.TRAIN,caffe.TEST])
-    blacklist = trainnet.gen_net(nclass, **kwargs)
-    trainnet.set_conv_params(blacklist=blacklist)
-    trainnet.euclideanloss('last_conv','last_conv2')
-    
-    with open('train.prototxt','w') as fout:
-        fout.write(trainnet.toproto());
-'''
 if __name__ == "__main__":
-    yolo_ts(20);
+    yolo_ts0(12);
--- experiments/yolov2.py
+++ experiments/yolov2.py
@@ -1,11 +1,16 @@
 import os.path as op
 from quickercaffe import *
-from qcmodels import TinyDarkNetV3, DarkNetMS, WeeNetV0
+from qcmodels import TinyDarkNetV3, DarkNetMS
 
                 
 def yolo_darknet(nclass):
     name = 'yolo-darknet'
-    head_cfg=[(1024,3,1),(1024,3,1),(1024,3,1)]
+    #head_cfg=[(1024,3,1),(1024,3,1),(1024,3,1)]  # 0.669 @10k iter, 
+    head_cfg=[(1024,1,1),(1024,3,4),(1024,1,1)]   #0.66.0218
+    head_cfg=[(1024,1,1),(1024,3,8),(1024,1,1)]   #0.655064 
+    #head_cfg=[(512,1,1),(1024,3,8)]*2  # 0.649049  @10k iter,   @20k iter
+    #head_cfg=[(1024,1,1),(1024,3,8)]  # 0.658813
+    #head_cfg=[(512,1,1),(1024,3,8)]*3 # 0.641684 @10k iter, 0.665731 @20k iter
     trainnet = DarkNet(name)
     datafolder = 'data'
     imgsize = 416
@@ -13,7 +18,7 @@ def yolo_darknet(nclass):
     trainnet.backbone()
     yolo_addhead(trainnet,nclass, deploy=False,head_cfg=head_cfg )
     trainnet.set_conv_params()
-    with open('train.prototxt','w') as fout:
+    with open('yolotrain.prototxt','w') as fout:
         fout.write(trainnet.toproto());
     testnet = DarkNet(name)    
     yolo_addinput(testnet,datafolder,nclass,imgsize,deploy=True)
@@ -82,7 +87,7 @@ def yolov3(nclass):
     name = 'yolov3'
     trainnet = DarkNet53(name)
     #head_cfg=[(512,1,1),(1024,3,1),(512,1,1),(1024,3,1),(512,1,1),(1024,3,1)]
-    head_cfg = [(1024,3,1), (1024,3,1), (1024,3,1)] 
+    head_cfg = [(1024,1,1), (1024,3,8), (1024,1,1)] 
     datafolder = 'data'
     imgsize = 416
     yolo_addinput(trainnet,datafolder,nclass,imgsize,deploy=False)
@@ -99,23 +104,27 @@ def yolov3(nclass):
         fout.write(testnet.toproto());        
         
 
-def yolo_weenet(nclass):
-    name = 'yolov3'
-    trainnet = WeeNetV0(name)
-    head_cfg=[(512,1,1),(1024,3,1),(512,1,1),(1024,3,1),(512,1,1),(1024,3,1)]
+def yolo_swift19(nclass):
+    name = 'yolo_swift19'
+    trainnet = SwiftNet(name)
+    # head_cfg=[(512,1,1),(1024,3,8),(512,1,1),(1024,3,8)]          54.33
+    # head_cfg=[(512,1,1),(1024,3,8),(512,1,1)]       52.9908 
+    # head_cfg=[(1024,1,1),(1024,3,8),(1024,1,1)]         53.54
     datafolder = 'data'
     imgsize = 416
-    yolo_addinput(trainnet,datafolder,nclass,imgsize,deploy=False, head_cfg=head_cfg)
+    yolo_addinput(trainnet,datafolder,nclass,imgsize,deploy=False)
     trainnet.backbone()
-    yolo_addreshead(trainnet,nclass, deploy=False)
+    yolo_addhead(trainnet,nclass, deploy=False, head_cfg=head_cfg)
     trainnet.set_conv_params()
     with open('yolotrain.prototxt','w') as fout:
         fout.write(trainnet.toproto());
-    testnet = WeeNetV0(name)    
-    yolo_addinput(testnet,datafolder,nclass,imgsize,deploy=True, head_cfg=head_cfg)
+    testnet = SwiftNet(name)    
+    yolo_addinput(testnet,datafolder,nclass,imgsize,deploy=True)
     testnet.backbone()
-    yolo_addreshead(testnet, nclass, deploy=True)  
+    yolo_addhead(testnet, nclass, deploy=True, head_cfg=head_cfg)  
     with open('test.prototxt','w') as fout:
         fout.write(testnet.toproto());                
 if __name__ == "__main__":
-    yolov3(12)
+    yolo_darknet(20)
+    #yolov3(12)
+
